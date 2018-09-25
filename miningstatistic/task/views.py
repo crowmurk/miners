0def7f97d25b17c37e0d0e5342dd0958f6442ee3
crowmurk <crowmurk@gmail.com>
2018-09-25 08:36:40 +0300

    Добавлена форма и представления для модели Config

--- miningstatistic/task/views.py
+++ miningstatistic/task/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    CreateView,
+    DetailView,
+    UpdateView,
+    DeleteView
+)
+
+from .models import Config
+from .forms import ConfigForm
 
 # Create your views here.
+
+class ConfigList(ListView):
+    model = Config
+    paginate_by = 7
+
+
+class ConfigCreate(CreateView):
+    model = Config
+    form_class = ConfigForm
+
+
+class ConfigDetail(DetailView):
+    model = Config
+    form_class = ConfigForm
+
+
+class ConfigUpdate(UpdateView):
+    model = Config
+    form_class = ConfigForm
+
+
+class ConfigDelete(DeleteView):
+    model = Config
+    success_url = reverse_lazy('task:config:list')
