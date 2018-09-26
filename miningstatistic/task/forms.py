f29338d1963e77eaa34f34b6d3721acdc05b0cc2
crowmurk <crowmurk@gmail.com>
2018-09-26 17:57:14 +0300

    Создание форм и представлений модели task.Server

--- miningstatistic/task/forms.py
+++ miningstatistic/task/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from .models import Config
+from .models import Config, Server
 
 class ConfigForm(forms.ModelForm):
     class Meta:
         model = Config
         fields = '__all__'
         exclude = ('slug', )
+
+class ServerForm(forms.ModelForm):
+    class Meta:
+        model = Server
+        fields = '__all__'
+        exclude = ('last_executed', 'status')
--- miningstatistic/task/views.py
+++ miningstatistic/task/views.py
@@ -7,8 +7,8 @@ from django.views.generic import (
     DeleteView
 )
 
-from .models import Config
-from .forms import ConfigForm
+from .models import Config, Server
+from .forms import ConfigForm, ServerForm
 
 # Create your views here.
 
@@ -35,3 +35,28 @@ class ConfigUpdate(UpdateView):
 class ConfigDelete(DeleteView):
     model = Config
     success_url = reverse_lazy('task:config:list')
+
+
+class ServerList(ListView):
+    model = Server
+    paginate_by = 7
+
+
+class ServerCreate(CreateView):
+    model = Server
+    form_class = ServerForm
+
+
+class ServerDetail(DetailView):
+    model = Server
+    form_class = ServerForm
+
+
+class ServerUpdate(UpdateView):
+    model = Server
+    form_class = ServerForm
+
+
+class ServerDelete(DeleteView):
+    model = Server
+    success_url = reverse_lazy('task:server:list')
