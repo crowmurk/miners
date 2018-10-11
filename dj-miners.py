80817c39896880c6e7227d7f00a2bc07613384bc
crowmurk <crowmurk@gmail.com>
2018-10-11 11:03:39 +0300

    Прводим ответы от майнеров к требуемому формату

--- dj-miners.py
+++ dj-miners.py
@@ -6,19 +6,27 @@
 # Загрузка результатов в БД - OK
 # TODO Отправка статистики Zabbix
 
-import sys
-import os
 import django
-import logging
 import json
+import logging
+import os
+import re
+import sys
 
-from systemd import journal
 from collections import namedtuple
+from copy import deepcopy
+from datetime import datetime
+from systemd import journal
 
 from pyminers import Sender
 
 
 class Worker():
+    """Извлекает настроки опроса майнеров
+    и отправки статистики Zabbix из БД Django.
+    Сохраняет результаты опроса в БД.
+    """
+
     def __init__(self, config='Default'):
         # Получаем общие настройки, как словарь
         self.__config = Config.objects.filter(
@@ -51,7 +59,7 @@ class Worker():
 
     @property
     def config(self):
-        """Настроки работы скрипта
+        """Общие настроки
         """
         try:
             properties = namedtuple(
@@ -139,6 +147,9 @@ class Worker():
     def save(self, data):
         """Добавляет результаты выполнения заданий в БД
         """
+        # Представляет ответ от майнера в требуемом формате
+        convert = Converter()
+
         # Получаем идентификатор опроса
         if ServerStatistic.objects.count():
             # Есть записи в таблице
@@ -171,12 +182,26 @@ class Worker():
                 'result': dict(),
             }
 
-            # Добавляем результаты для сохранения
+            # Добавляем результаты запросов
             for line in task_data['Exchange']:
+                if line['Error']:
+                    # Если любой из запросов завершился ошибкой
+                    # добавляем только ответ с описанием ошибки
+                    data['result'] = line['Response']
+                    break
                 for request in task.requests.values('name', 'request'):
+                    # Определяем имя запроса по телу запроса из ответа
                     if line['Request'] == json.loads(request['request']):
+                        # Добавляем в виде {'RequestName': Response, }
                         data['result'][request['name']] = line['Response']
                         break
+
+            # Если запрос не завершился ошибкой
+            # преобразуем результаты к требуемому формату
+            if data['status']:
+                data['result'] = convert(task.server.miner.name, data['result'])
+
+            # Форма ожидает строку а не объект
             data['result'] = json.dumps(data['result'])
 
             # Форма обеспечивает проверку данных
@@ -206,6 +231,192 @@ class Worker():
                         error=(form.errors, ),
                     ),
                 )
+
+
+class Converter():
+    """Приводит ответы от майнеров к требуемому формату
+    """
+
+    def __init__(self):
+        # Методы для преобразования ответов майнеров
+        self.__supported_miners = {
+            "Antminer S9 CGMiner": self.__cGMiner,
+            "Claymore's CryptoNote": self.__etherium,
+            "Claymore's Dual Ethereum": self.__etherium,
+            "EWBF's CUDA ZCash": self.__zCash,
+        }
+
+    def __call__(self, miner, data):
+        """Приводит ответы от майнеров к требуемому формату
+        """
+        if miner in self.miners:
+            return self.__supported_miners[miner](data)
+        else:
+            raise ValueError(
+                "Unknown miner: '{miner}'".format(miner=miner),
+            )
+
+    @property
+    def miners(self):
+        """Поддерживаемые майнеры"""
+        return self.__supported_miners.keys()
+
+    @staticmethod
+    def __cGMiner(value):
+        """Модифицирует ответ от сервера CGMiner
+        """
+        try:
+            summary = deepcopy(value['Summary']['SUMMARY'][0])
+            stats = deepcopy(value['Stats']['STATS'][0])
+        except KeyError as e:
+            raise ValueError("Invalid response:", e) from None
+
+        # Elapsed time
+        m, _ = divmod(summary['Elapsed'], 60)
+        h, m = divmod(m, 60)
+        d, h = divmod(h, 24)
+        summary['Elapsed'] = '{}d {:02}:{:02}'.format(d, h, m)
+
+        summary['Last getwork'] = datetime.fromtimestamp(
+            summary['Last getwork'],
+        ).strftime("%Y.%m.%d-%H:%M:%S")
+
+        # Шаблоны поиска
+        templates = {
+            'Fans': '^fan[0-9]+$',
+            'Temps 1': '^temp[0-9]+$',
+            'Temps 2': '^temp2_[0-9]+$',
+            'Temps 3': '^temp3_[0-9]+$',
+            'Freq av': '^freq_avg[0-9]+$',
+            'Chainrate': '^chain_rateideal[0-9]+$',
+        }
+        templates = {key: re.compile(item) for key, item in templates.items()}
+
+        # Ищем поля по шаблону и объединяем данные
+        summary.update(
+            {key: ', '.join(
+                [str(item)
+                 for _, item in stats.items()
+                 if bool(template.match(_)) and item])
+             for key, template in templates.items()},
+        )
+
+        summary['Description'] = '{miner} - {version}'.format(
+            miner=stats['Type'],
+            version=stats['Miner'],
+        )
+
+        return {key: summary[key]
+                for key in [
+                    'Elapsed', 'Accepted', 'Rejected',
+                    'GHS av', 'Hardware Errors', 'Discarded',
+                    'Device Rejected%', 'Pool Rejected%',
+                    'Last getwork', 'Fans', 'Temps 1',
+                    'Temps 2', 'Description']
+                }
+
+    @staticmethod
+    def __zCash(value):
+        """Модифицирует ответ от сервера ZCash
+        """
+        try:
+            value = deepcopy(value['Statistic'])
+        except KeyError as e:
+            raise ValueError("Invalid response:", e) from None
+
+        # Расшифровка статусов состояния GPU
+        gpuStatus = {
+            0: 'launched',
+            1: 'prepare',
+            2: 'work',
+            3: 'stopped'
+        }
+
+        """Ответ от сервера приходит в виде списка словарей,
+        где каждый словарь - статистика работы по одному GPU
+        """
+
+        # Преобразуем список слоарей в словарь со списками
+        value = dict(zip(value[0], zip(*[item.values() for item in value])))
+        # Расшифровываем статусы GPU
+        value['gpu_status'] = [gpuStatus[item] for item in value['gpu_status']]
+        # Преобразуем списки в строки значений разделенные ';'
+        value = {key: '; '.join([str(item) for item in value[key]])
+                 for key in value}
+        # Переименовываем и возвращаем только необходимые поля
+        return {val: value[key]
+                for key, val in {
+                    'gpu_status': 'GPU status',
+                    'temperature': 'Temperatures',
+                    'gpu_power_usage': 'GPU power usage',
+                    'speed_sps': 'Solution / second',
+                    'accepted_shares': 'Accepted shares',
+                    'rejected_shares': 'Rejected shares'}.items()
+                }
+
+    @staticmethod
+    def __etherium(value):
+        """Модифицирует ответ от сервера Etherium
+        """
+        try:
+            value = deepcopy(value['Statistic'])
+        except KeyError as e:
+            raise ValueError("Invalid response:", e) from None
+
+        # Uptime
+        h, m = divmod(value['Uptime'], 60)
+        d, h = divmod(h, 24)
+        value['Uptime'] = '{}d {:02}:{:02}'.format(d, h, m)
+
+        # Статистика ETH / DCR
+        for key in ['ETH', 'DCR']:
+            try:
+                percent = round(
+                    value[key]['Rejected'] * 100 / value[key]['Shares'], 2
+                )
+            except ZeroDivisionError:
+                percent = 0
+
+            value[key] = '{T} GH/s, {A}/{R} ({P}%)'.format(
+                T=value[key]['Hashrate'] / 1000,
+                A=value[key]['Shares'],
+                R=value[key]['Rejected'],
+                P=percent,
+            )
+
+        value['ETH / GPU'] = '; '.join(
+            [str(item) for item in value['ETH Detailed']]
+        )
+        value['DCR / GPU'] = '; '.join(
+            [str(item) for item in value['DCR Detailed']]
+        )
+
+        # Параметры GPU
+        value['GPU'] = ' '.join(
+            ['({T}C:{S}%)'.format(
+                T=item[0],
+                S=item[1])
+             for item in zip(
+                 value['GPU']['Temperatures'],
+                 value['GPU']['Fan Speeds'],)
+             ]
+        )
+
+        # Информация по pool'ам
+        value['Pools'] = ' '.join(
+            ['{A}:{P}'.format(
+                A=item['Host'],
+                P=item['Port'])
+             for item in value['Pools']]
+        )
+
+        # Возвращаем только необходимые поля
+        return {key: value[key]
+                for key in [
+                    'Uptime', 'ETH', 'ETH / GPU',
+                    'DCR', 'DCR / GPU', 'GPU', 'Pools']
+                }
+
 
 def main():
     # Загружаем задания из БД
