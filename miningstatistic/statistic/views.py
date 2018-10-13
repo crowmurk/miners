6a74833e859103a4d3255234d97e671e4313d371
crowmurk <crowmurk@gmail.com>
2018-10-14 00:29:23 +0300

    Динамическое обновление данных в таблицах

--- miningstatistic/statistic/views.py
+++ miningstatistic/statistic/views.py
@@ -15,27 +15,27 @@ from .tables import (
 
 class ServerStatisticList(MultiTableMixin, TemplateView):
     template_name = 'statistic/serverstatistic_list.html'
-    model = ServerStatistic
     tables = [
         ServerStatisticErrorTable,
         ServerStatisticEtheriumTable,
         ServerStatisticZCashTable,
         ServerStatisticCGMinerTable,
     ]
-    tables_data = [
-        model.objects.results_last().filter(
-            status=False,
-        ).table_data(),
-        model.objects.results_last().filter(
-            status=True,
-            task__server__miner__slug='claymores-dual-ethereum-amd-gpu-98',
-        ).table_data(),
-        model.objects.results_last().filter(
-            status=True,
-            task__server__miner__slug='ewbfs-cuda-zcash-034b',
-        ).table_data(),
-        model.objects.results_last().filter(
-            status=True,
-            task__server__miner__slug='antminer-s9-cgminer-490',
-        ).table_data(),
-    ]
+
+    def get_tables_data(self):
+        tables_data = [
+            ServerStatistic.objects.results_last().filter(
+                status=False,
+            ).table_data(),
+        ]
+        for miner_slug in (
+                'claymores-dual-ethereum-amd-gpu-98',
+                'ewbfs-cuda-zcash-034b',
+                'antminer-s9-cgminer-490',
+        ):
+            tables_data.append(
+                ServerStatistic.objects.results_last().filter(
+                    status=True,
+                    task__server__miner__slug=miner_slug
+                ).table_data())
+        return tables_data
