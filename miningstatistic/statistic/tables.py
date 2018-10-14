75236dedbdfb152a0da695c67bfb156349b5ac85
crowmurk <crowmurk@gmail.com>
2018-10-14 21:01:09 +0300

    Генерация таблиц статистики на основе данных опроса майнеров

--- miningstatistic/statistic/tables.py
+++ miningstatistic/statistic/tables.py
@@ -1,19 +1,21 @@
 import django_tables2 as tables
 
 
-class ServerStatisticTableMixin(tables.Table):
+class ServerStatisticTable(tables.Table):
     server = tables.Column(
         linkify=(('miner:server:detail', {'slug': tables.A('server')})),
-        verbose_name='Сервер',
     )
+
+    def __init__(self, *args, **kwargs):
+        self.verbose_name_prefix = kwargs.pop('verbose_name_prefix', None)
+        self.miner = kwargs.pop('miner', None)
+        super().__init__(*args, **kwargs)
 
     def render_server(self, value):
         return value.upper()
 
 
-class ServerStatisticErrorTable(ServerStatisticTableMixin, tables.Table):
-    verbose_name = 'Ошибки'
-
+class ServerStatisticErrorTable(ServerStatisticTable, tables.Table):
     error_type = tables.Column(
         verbose_name='Тип ошибки',
     )
@@ -22,79 +24,4 @@ class ServerStatisticErrorTable(ServerStatisticTableMixin, tables.Table):
     )
     error_message = tables.Column(
         verbose_name='Сообщение',
-    )
-
-
-class ServerStatisticEtheriumTable(ServerStatisticTableMixin, tables.Table):
-    verbose_name = 'Статистика Etherium'
-
-    uptime = tables.Column()
-    eth = tables.Column(
-        verbose_name='ETH',
-    )
-    eth_per_gpu = tables.Column(
-        verbose_name='ETH/GPU',
-    )
-    dcr = tables.Column(
-        verbose_name='DCR',
-    )
-    dcr_per_gpu = tables.Column(
-        verbose_name='DCR/GPU',
-    )
-    gpu = tables.Column(
-        verbose_name='Параметры GPU',
-    )
-    pools = tables.Column(
-        verbose_name='Пулы',
-    )
-
-
-class ServerStatisticCGMinerTable(ServerStatisticTableMixin, tables.Table):
-    verbose_name = 'Статистика CGMiner'
-
-    elapsed = tables.Column(
-        verbose_name='Uptime',
-    )
-    accepted = tables.Column()
-    rejected = tables.Column()
-    ghs_av = tables.Column(
-        verbose_name='GHS среднее',
-    )
-    hardware_errors = tables.Column()
-    discarded = tables.Column()
-    dev_pool_rejected = tables.Column(
-        verbose_name='Dev/Pool Rejected (%)',
-    )
-    last_getwork = tables.Column()
-    fans = tables.Column()
-    temps_1 = tables.Column()
-    temps_2 = tables.Column()
-    description = tables.Column(
-        verbose_name='Описание',
-    )
-
-    class Meta:
-        exclude = ('description', )
-
-
-class ServerStatisticZCashTable(ServerStatisticTableMixin, tables.Table):
-    verbose_name = 'Статистика ZCash'
-
-    gpu_status = tables.Column(
-        verbose_name='GPU Статус',
-    )
-    temperature = tables.Column(
-        verbose_name='Температура',
-    )
-    gpu_power_usage = tables.Column(
-        verbose_name='GPU Потребление',
-    )
-    speed_sps = tables.Column(
-        verbose_name='Решено/сек.',
-    )
-    accepted_shares = tables.Column(
-        verbose_name='Accepted Shares',
-    )
-    rejected_shares = tables.Column(
-        verbose_name='Rejected Shares',
     )
--- miningstatistic/statistic/views.py
+++ miningstatistic/statistic/views.py
@@ -1,47 +1,77 @@
+import json
+
 from django.utils import timezone
 from django.views.generic.base import TemplateView
 
-from django_tables2 import MultiTableMixin
+from django_tables2 import MultiTableMixin, Column
 
 from task.models import Config
+from miner.models import Miner
 
 from .models import ServerStatistic
 from .tables import (
     ServerStatisticErrorTable,
-    ServerStatisticEtheriumTable,
-    ServerStatisticZCashTable,
-    ServerStatisticCGMinerTable,
+    ServerStatisticTable
 )
 
 # Create your views here.
 
-
 class ServerStatisticList(MultiTableMixin, TemplateView):
     template_name = 'statistic/serverstatistic_list.html'
-    tables = [
-        ServerStatisticErrorTable,
-        ServerStatisticEtheriumTable,
-        ServerStatisticZCashTable,
-        ServerStatisticCGMinerTable,
-    ]
 
-    def get_tables_data(self):
-        tables_data = [
-            ServerStatistic.objects.results_last().filter(
+    def get_tables(self):
+        def get_data_list(data):
+            """Возвращает список с данными
+            для наполнения таблицы
+            """
+            data_list = []
+            for server, result in data.values_list(
+                    'task__server__slug',
+                    'result',
+            ):
+                result = json.loads(result)
+                result['server'] = server
+                data_list.append(result)
+            return data_list
+
+        # Список таблиц для представления
+        tables = []
+
+        # Результаты последнего опроса
+        last_data = ServerStatistic.objects.results_last()
+
+        for miner in Miner.objects.all():
+            # Сооздаем таблицу с ошибками
+            data = last_data.filter(
                 status=False,
-            ).table_data(),
-        ]
-        for miner_slug in (
-                'claymores-dual-ethereum-amd-gpu-98',
-                'ewbfs-cuda-zcash-034b',
-                'antminer-s9-cgminer-490',
-        ):
-            tables_data.append(
-                ServerStatistic.objects.results_last().filter(
-                    status=True,
-                    task__server__miner__slug=miner_slug
-                ).table_data())
-        return tables_data
+                task__server__miner=miner,
+            )
+            if data:
+                table = ServerStatisticErrorTable(
+                    get_data_list(data),
+                    verbose_name_prefix='Ошибки',
+                    miner=miner,
+                )
+                tables.append(table)
+
+            # Создаем таблицу со статистикой
+            data = last_data.filter(
+                status=True,
+                task__server__miner=miner,
+            )
+            if data:
+                data = get_data_list(data)
+                table = ServerStatisticTable(
+                    data,
+                    extra_columns=[(name, Column())
+                                   for name in data[0].keys()
+                                   if name != 'server'],
+                    verbose_name_prefix='Статистика',
+                    miner=miner,
+                )
+                tables.append(table)
+
+        return tables
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
