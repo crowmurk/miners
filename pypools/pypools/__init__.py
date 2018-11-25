a92e35700f791d4664e76f7d5e793e32e334e004
crowmurk <crowmurk@gmail.com>
2018-11-25 08:59:32 +0300

    Стилистические правки

--- pypools/pypools/__init__.py
+++ pypools/pypools/__init__.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 
+"""Модуль для опроса пулов.
+Позволяет отправлять запросы к пулам в формате json.
+Подерживаются запросы к пулам:
+    'NanoPool - nanopool.org' - Etherium, Monero
+    'FlyPool - flypool.org' - ZCash
 """
-Модуль для опроса пулов.
-    Позволяет отправлять запросы к пулам в формате json.
-        Подерживаются запросы к пулам:
-            'NanoPool - nanopool.org' - Etherium, Monero
-            'FlyPool - flypool.org' - ZCash"""
 
 from .pool import Pool
 from .flypool import FlyPool
 from .nanopool import NanoPool
 from .sender import Sender
-
 
 __all__ = ['Pool', 'FlyPool', 'NanoPool', 'Sender']
 
--- pypools/setup.py
+++ pypools/setup.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+import setuptools
+import pypools
 
-setup(name='pypools',
-      version='1.0.2',
-      description='Request miners pools',
-      author='varga',
-      classifiers=['Development Status :: 5 - Production/Stable',
-                   'Programming Language :: Python :: 3.4',
-                   'Natural Language :: Russian',
-                   'Operating System :: POSIX :: Linux'],
-      packages=['pypools'],
-      install_requires=['systemd', 'json2html', 'configobj',
-                        'py-zabbix', 'validictory'],
-      python_requires='~=3.4')
+setuptools.setup(
+    name='pypools',
+    version=pypools.__version__,
+    author=pypools.__author__,
+    description='Request miners pools',
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'Programming Language :: Python :: 3.4',
+                 'Natural Language :: Russian',
+                 'Operating System :: POSIX :: Linux'],
+    packages=setuptools.find_packages(),
+    install_requires=['systemd', 'json2html', 'configobj',
+                      'py-zabbix', 'validictory'],
+    python_requires='~=3.4',
+)
